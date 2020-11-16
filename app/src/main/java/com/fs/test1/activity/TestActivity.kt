f92fab7bf9723c363f50b0dd1933e06bdfb863c8
Stardust009 <15540922270@163.com>
2020-11-16 10:13:28 +0800

    commit

--- app/src/main/java/com/fs/test1/activity/TestActivity.kt
+++ app/src/main/java/com/fs/test1/activity/TestActivity.kt
@@ -16,40 +16,5 @@ class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.downloading_page_layout)
-
-
-        if (Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED)
-
-            getExternalFilesDir(null)?.let { DEFAULT_DOWNLOAD_PATH = it.absolutePath }
-        downloading_task_list.apply {
-            mAdapter =
-                DownloadItemListAdapter(this@TestActivity, TaskManager.getDownloadingTaskList())
-            adapter = mAdapter
-            layoutManager =
-                LinearLayoutManager(this@TestActivity, LinearLayoutManager.VERTICAL, false)
-        }
-
-        with(TaskManager) {
-//            setDownloadTaskListAdapter(mAdapter as DownloadItemListAdapter)
-            updateDataSourceListener = {
-                this@TestActivity.runOnUiThread {
-                    mAdapter.notifyDataSetChanged()
-                    downloading_task_list.scrollToPosition(mAdapter.itemCount - 1)
-                    Log.d(TAG, "更新。。。${Thread.currentThread().name}")
-                }
-            }
-        }
-
-        with(TaskManager) {
-            setDownloadTaskListAdapter(mAdapter)
-            updateDataSourceListener = {
-                runOnUiThread {
-                    mAdapter.notifyDataSetChanged()
-                    downloading_task_list.scrollToPosition(mAdapter.itemCount - 1)
-                    Log.d(TAG, "更新。。。${Thread.currentThread().name}")
-                }
-            }
-        }
-
     }
 }
